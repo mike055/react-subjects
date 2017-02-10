2f92084233b8dda833fa53bc6d5bcc634fae230b
Aaron Lewis <Aaron Lewis>
2017-02-10 12:01:07 +1100

    mini redux exercide

--- subjects/MiniRedux/exercise/mini-redux/Provider.js
+++ subjects/MiniRedux/exercise/mini-redux/Provider.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React  from 'react'
 
 class Provider extends React.Component {
+
+  static childContextTypes = {
+    store: React.PropTypes.object.isRequired
+  }
+
+  static propTypes = {
+    store: React.PropTypes.object.isRequired
+  }
+
+  getChildContext() {
+
+   return {
+    store: this.props.store
+   }
+ }
+
   render() {
     return <div>{this.props.children}</div>
   }
--- subjects/MiniRedux/exercise/mini-redux/connect.js
+++ subjects/MiniRedux/exercise/mini-redux/connect.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 const connect = (mapStateToProps) => {
   return (Component) => {
-    return Component
+    return class extends React.Component {
+      static contextTypes = {
+        store: React.PropTypes.object.isRequired
+      }
+
+      componentWillMount() {
+        this.context.store.subscribe(()=> {
+          console.log('do something')
+          this.forceUpdate()
+        }); 
+      }
+
+      render() {
+        const data = mapStateToProps(this.context.store.getState())
+
+        return <Component {...this.props} {...data} dispatch={this.context.store.dispatch}/>
+      }
+    }
   }
 }
 
